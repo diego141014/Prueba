0d8acdb63060c82f9b28e779eef81c165a913002
diego141014 <diego.clase14@gmail.com>
2025-05-06 20:44:15 +0200

    Update prueba.java

--- prueba.java
+++ prueba.java
@@ -1,5 +1,5 @@
 public class prueba {
     public static void main(String[] args) {
-        System.out.println("Esto es una prueba");
+        System.out.println("Esto es una prueba, modificado");
     }
 }
